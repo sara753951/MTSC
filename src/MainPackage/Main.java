2e57a9da7a07b767be6aba7646defe7746d2c079
= <=>
2018-01-16 15:20:08 +0800

    new a project

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("HelloWorld");
+        int a = 10;
+        int b = 2;
+        System.out.println(a / b);
     }
 }
